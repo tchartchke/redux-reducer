e371a1bee973dd03bb2b5df97f441caa826e89fe
tchartchke <chartai.kl@gmail.com>
2021-06-21 11:15:48 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends : []}, action){
+  switch (action.type){
+    case 'ADD_FRIEND' : return { friends : [...state.friends, action.friend] };
+    case 'REMOVE_FRIEND' : return { friends : addAllExcept(action.id, state.friends) };
+    default : return { friends : state.friends };
+  }
+}
+
+function addAllExcept(id, friends){
+  const listOfFriends = [];
+  for (const friend of friends){
+    if (friend.id !== id){ listOfFriends.push(friend) }
+  }
+  return listOfFriends;
 }
